cb23ddc7b4734f8d347608f2e63b048ecba56ca3
yoowonyoung <wonyoung0105@gmail.com>
2017-05-27 12:54:06 +0900

    메인 프레임 및 파일 아이콘

--- MIDASChallenge/src/MainFrame.java
+++ MIDASChallenge/src/MainFrame.java
@@ -1,4 +1,10 @@
+import java.awt.event.KeyEvent;
+
+import javax.swing.ImageIcon;
 import javax.swing.JFrame;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
 
 /**
  * 화면을 구성하게될 메인 프레임, 상단목록과 좌측 사이드바, 편집화면을 가지게됨
@@ -6,5 +12,48 @@ import javax.swing.JFrame;
  *
  */
 public class MainFrame extends JFrame{
-
+	private int width = 1280;
+	private int height = 1024;
+	
+	public MainFrame() {
+		initUI();
+	}
+	
+	/**
+	 * UI를 초기화 하는 메서드
+	 */
+	public void initUI() {
+		setSize(width, height);
+		createMenuBar();
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	    setResizable(false);
+		setVisible(true);
+	    setTitle("MIDAS_UML");
+	}
+	
+	/**
+	 * 메뉴바를 만드는 메서드
+	 */
+	public void createMenuBar() {
+		JMenuBar menuBar = new JMenuBar();
+		JMenu file = new JMenu("File");
+		ImageIcon fileIcon = new ImageIcon(System.getProperty("user.dir")+"\\images\\fileIcon.png");
+		file.setMnemonic(KeyEvent.VK_F);
+		file.setIcon(fileIcon);
+		JMenuItem newFile = new JMenuItem("New");
+		newFile.setToolTipText("새로 파일을 만듭니다");
+		JMenuItem openFile = new JMenuItem("Open File");
+		openFile.setToolTipText("파일을 불러옵니다");
+		JMenuItem saveFile = new JMenuItem("Save");
+		saveFile.setToolTipText("파일을 저장합니다");
+		JMenuItem saveAsFile = new JMenuItem("Save As...");
+		saveAsFile.setToolTipText("파일을 다른 이름으로 저장합니다");
+		file.add(newFile);
+		file.add(openFile);
+		file.add(saveFile);
+		file.add(saveAsFile);
+		menuBar.add(file);
+		setJMenuBar(menuBar);
+	}
+	
 }
